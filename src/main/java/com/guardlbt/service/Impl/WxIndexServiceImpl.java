23cca4de3641dcdeb00364e938dcd13a403e5e02
hejz <75412985@qq.com>
2021-08-27 10:42:29 +0800

    djj项目小程序后台代码

--- src/main/java/com/guardlbt/service/Impl/WxIndexServiceImpl.java
+++ src/main/java/com/guardlbt/service/Impl/WxIndexServiceImpl.java
@@ -29,7 +29,7 @@ import java.util.stream.Collectors;
 @Service
 public class WxIndexServiceImpl implements WxIndexService {
 
-    public static final String ACCESS_TOKEN = "access_token::";
+    public static final String ACCESS_TOKEN = "access_token";
     @Autowired
     private WxConfigurationProperties properties;
     @Autowired
